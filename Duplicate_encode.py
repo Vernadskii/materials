5e2d899af094d3d4128ad21e5686d69a76aa56fb
Vernadsky Danya <d.vernadsky@yandex.ru>
2020-09-09 23:44:28 +0300

    String exercise

--- Duplicate_encode.py
+++ Duplicate_encode.py
@@ -15,4 +15,8 @@ def duplicate_encode(word):
             result += '('
     return result
 
+def duplicate_encode1(word):
+    return "".join(["(" if word.lower().count(c) == 1 else ")" for c in word.lower()])
+
 print(duplicate_encode("Success"))
+print(duplicate_encode1("Success"))
